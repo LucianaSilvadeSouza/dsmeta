4996aa388778b5f8ff08559f359cdbf4266ade6c
LucianaSilvadeSouza <lucisouzasilva6@gmail.com>
2022-07-15 15:38:25 -0300

    Sale listing

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header()  {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/luciana_souza0202">@luciana_souza0202</a>
+                    <a href="https://www.instagram.com/luciana_souza0202"> @luciana_souza0202</a>
                 </p>
             </div>
         </header>          
---
+++ frontend/src/components/models/sale.ts
@@ -0,0 +1,20 @@
+
+export type Sale = {
+    id: number; 
+    sellerName: string; 
+    date: string;
+    visited: number; 
+    deals: number;
+    amount: number;
+    
+}
+
+
+
+
+
+
+    
+
+
+   
